e552765e5cee17807ec5b8dbe58c92063b1e3bc4
Riccardo Bassani <riccardo.bassani@gasweb.it>
2023-01-12 11:09:04 +0100

    Aggiungere i file di progetto.

---
+++ xTestNetBridgeArca/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xTestNetBridgeArca
+{
+    public class Programma
+    {
+
+        public async Task<string> HelloWorldAsync()
+        {
+            await Task.Delay(1);
+            return "Hello from async!";
+        }
+
+        public string HelloWorldAsync4VFP()
+        {
+            var retCall = HelloWorldAsync();
+            retCall.Wait();
+            return retCall.Result;
+        }
+
+    }
+}
